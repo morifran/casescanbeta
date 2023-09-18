ea316e00be4ac30431749001122d496d561ca842
morifran <82995026+morifran@users.noreply.github.com>
2023-09-18 19:59:44 +0300

    saved
    мне просто страшно, что что-то сломается

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,10 +1,22 @@
-
+import logo from "../pics/scanlogo.png"
+import menu from "../pics/menu.svg"
 
 function Header() {
-    return (<Header>
-    <p>ghb</p>
-     </Header>
-    );
+    return (<div className="headerDiv">
+    <img src={logo} alt="Лого компании скан" className="logo"></img>
+    <ul className="headerList none">
+      <li><a>Главная</a></li>
+      <li><a>Тарифы</a></li>
+      <li><a>FAQ</a></li>
+    </ul>
+    <div className="userInfo"></div>
+    <div className="regMenu none">
+      <a className="register">Зарегистрироваться</a>
+      <p className="rectangle">│</p>
+      <button className="logIn">Войти</button>
+    </div>
+    <img src={menu} alt="изображение выпадающего меню" className="menu None"></img>
+    </div>);
   }
   
   export default Header;
