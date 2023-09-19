b15c6b0d02e4420e89a4b0bec4019188724b5ad7
morifran <82995026+morifran@users.noreply.github.com>
2023-09-19 18:58:14 +0300

    save2
    я паникер

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -7,7 +7,7 @@ function Main() {
         <div className="description">
             <h2>сервис по поиску публикаций о компании по его ИНН</h2>
             <p className="commonText">Комплексный анализ публикаций, получение данных в формате PDF на электронную почту.</p>
-            <div className="butDiv"><button className="requestBut None">Запросить данные</button></div>
+            <div className="butDiv"><button className="requestBut ">Запросить данные</button></div>
         </div>
         <div className="imgDiv">
             <img src={TechGuy} alt="Гик" className="TechGuy"></img>
