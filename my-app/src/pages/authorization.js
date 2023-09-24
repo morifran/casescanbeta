b55c2df07a54d21f21a1209282f401f9d7d2c795
morifran <82995026+morifran@users.noreply.github.com>
2023-09-24 16:52:48 +0300

    save

--- my-app/src/pages/authorization.js
+++ my-app/src/pages/authorization.js
@@ -3,17 +3,22 @@ import Characters from "../pics/Characters.svg"
 import Google from "../pics/Google.svg"
 import Facebook from "../pics/facebook.svg"
 import Yandex from "../pics/Yandex.svg"
+import Lock from "../pics/lock.svg"
 
 export default class Autorization extends Component {
     render(){
         return(
             <div className='authorization'>
+                
                 <div className='decoration'>
                     <h2>Для оформления подписки на тариф, необходимо авторизоваться.</h2>
-                    <img src={Characters} className='character'/>
+                    <img src={Characters} className='character none'/>
                 </div>
+                
                 <div className='validation'>
-                    <div className='LogOrReg'>
+                    
+                    <div className='LogOrReg'> 
+                    <div className='forLock'><img src={Lock} className='lock'></img></div>
                         <button className='underlinedLog'>Войти</button>
                         <button className='underlinedReg'>Зарегистрироваться</button>
                     </div>
@@ -21,7 +26,7 @@ export default class Autorization extends Component {
                         <p className='registerP'>Логин или номер телефона:</p>
                         <input className='email'></input>
                         <p className='registerP'>Пароль:</p>
-                        <input className='password'></input>
+                        <input className='password' type='password'></input>
                         <button className='logInto'>Войти</button>
                         <a className='restorePassword' href='#'>Восстановить пароль</a>
                         <p className='registerP'>Войти через:</p>
@@ -32,6 +37,7 @@ export default class Autorization extends Component {
                         </div>
                     </div>
                 </div>
+                <img src={Characters} className='character None'></img>
             </div>
         )
     }
